0dd97d9011b7310c34d21eff451625fd8faf7451
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-10-10 15:15:16 +0300

    Home view implemented

---
+++ src/views/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+    return (
+        <div className='homeCSS'>
+            <h1>Welcome to Marwan Casino</h1>
+            <p>Your favorite place to play and win!</p>
+        </div>
+    );
+};
+
+export default Home;
